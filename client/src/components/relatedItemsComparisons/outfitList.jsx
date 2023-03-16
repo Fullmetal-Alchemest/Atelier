593efc964eb9d2e81d7fa01836188aff93cf1953
Tucker Williams <thrack.gorsh@gmail.com>
2023-03-15 17:29:11 -0700

    Added TODOs to each component

--- client/src/components/relatedItemsComparisons/outfitList.jsx
+++ client/src/components/relatedItemsComparisons/outfitList.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function OutfitList() {
-  // TODO:
+  // TODO: create a list of product card, taken from the user's
+  // outfit list in the db, this should be a carousel
+  // the action button for this list should remove the card from the list
   return (
     <h1>hello world</h1>
   );
--- client/src/components/relatedItemsComparisons/relatedProductsList.jsx
+++ client/src/components/relatedItemsComparisons/relatedProductsList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default function RelatedProductList() {
-  // TODO:
+  // TODO: create a list of related product cards, based on a list provided
+  // by the api db. This should be a carousel
   return (
     <h1>hello world</h1>
   );
