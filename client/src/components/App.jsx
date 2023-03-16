928a6fa1bef2598b5d6031db542bc38f19572ffb
Tucker Williams <thrack.gorsh@gmail.com>
2023-03-16 15:36:13 -0700

    Moved relatedItems components into proper folder

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ProductOverview from './productOverview/index.jsx';
 import RatingsReviews from './ratingsReviews/index.jsx';
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-=======
-import ReactDOM from 'react-dom/client';
->>>>>>> 1299662b01821a276e9d20c52dc83de923ee061f
 
 import { Provider } from 'react-redux';
 import store from '@lib/store.js';
-
-import App from '@components/App.jsx';
+// eslint-disable-next-line import/extensions
+import App from './components/App.jsx';
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
